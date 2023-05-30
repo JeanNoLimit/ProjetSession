949b3b6d8227b03372f8d8bc7fcef9dd163e1da1
Jean-Noel <jean.noel.richert@gmail.com>
2023-05-30 23:28:16 +0200

    création de la vue adin du site. (à compléter)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -30,9 +32,34 @@ class SecurityController extends AbstractController
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 
+
+    /**
+     * Affichage de la vue Profil utilisateur
+     *
+     * @return Response
+     */
     #[Route(path: '/security/viewProfile', name: 'view_profile')]
     public function viewProfile(): Response
     {
         return $this->render('security/viewProfile.html.twig');
     }
+
+
+    /**
+     * Affichage de la page administration. Permet de gérer les comptes utilisateurs, modifier leurs rôles, supprimer un compte.
+     *
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
+    #[Route(path: 'security/admin', name: 'administration')]
+    public function administration(EntityManagerInterface $em):Response
+    {
+
+        $users= $em->getRepository(User::class)->findBy([],['nom'=>'ASC', 'prenom'=>'ASC']);
+
+        return $this->render('security/admin.html.twig',[
+            'users' => $users
+        ]);
+    }
+
 }
